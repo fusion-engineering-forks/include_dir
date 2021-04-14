ed1c6dafce0b9a3effb87e5dd59b72f5f6f0e1f9
Aelis Sagot(Torsina) <aelis.sagot@gmail.com>
2021-04-14 10:01:59 +0200

    try to replace "\\\\" with "\\" in the cross platform path function to make it work

--- include_dir_impl/src/utils.rs
+++ include_dir_impl/src/utils.rs
@@ -3,9 +3,9 @@ use std::path::PathBuf;
 /**/
 pub fn create_cross_platform_path(path: &PathBuf) -> String {
     if cfg!(windows) && !cfg!(target_os = "windows") {
-        path.display().to_string().replace("\\\\", "/")
+        path.display().to_string().replace("\\", "/")
     } else if !cfg!(windows) && cfg!(target_os = "windows") {
-        path.display().to_string().replace("/", "\\\\")
+        path.display().to_string().replace("/", "\\")
     }
     else {
         path.display().to_string()
